c58fa8bd46a2de87cf5d54d399779a41f229e20c
Ashish Ranjan <ashish@tug.ro>
2020-03-28 03:42:52 +0530

    feat(alias): clear, less, man, nix, navigation

--- base.nix
+++ base.nix
@@ -4,5 +4,26 @@
   tug =
     { config, pkgs, ... }:
     {
+      environment = {
+
+        shellAliases = {
+          ".." = "cd ..";
+          "..." = "cd ../..";
+          "...." = "cd ../../..";
+          "cd.." = "cd ..";
+
+          c = "clear";
+          l = "less";
+          ll = "ls -hlA";
+          m = "man";
+
+          xb = "nix-build";
+          xc = "nix-channel";
+          xe = "nix-env";
+          xs = "nix-shell";
+          xt = "nix-store";
+        };
+
+      };
     };
 }
