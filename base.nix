b88c93cf05507efe4c6fefc7b9360543709a8f1f
Ashish Ranjan <ashish@tug.ro>
2020-03-28 04:00:32 +0530

    feat(env): Add some variables for a good shell experience

--- base.nix
+++ base.nix
@@ -35,6 +35,31 @@
           pkgs.vim
         ];
 
+        variables = {
+          EDITOR = "vim";
+
+          HISTCONTROL = "ignoredups"; # Ignore commands that are duplicates
+          HISTFILESIZE = "5000";      # Lines in history file
+          HISTIGNORE = "c:clear:exit:pwd";  # Ignore some commands from history file
+          HISTTIMEFORMAT = "%F %T ";  # Record timestamp of each command
+
+          LC_ALL = "en_US.UTF-8";
+
+          # Highlight searches and the first unread line after scrolling
+          # Ignore case
+          # Squeeze consecutive blank lines into a single blank line
+          # Set tab width to 4
+          MANPAGER = "less --hilite-search --HILITE-UNREAD --ignore-case --squeeze-blank-lines --tabs=4";
+
+          # Make new shells get the history lines from all previous
+          # shells instead of the default "last window closed" history
+          PROMPT_COMMAND = "history -a;";
+
+          XDG_CACHE_HOME = "$HOME/.cache";
+          XDG_CONFIG_HOME = "$HOME/.config";
+          XDG_DATA_HOME = "$HOME/.local/share";
+        };
+
       };
     };
 }
